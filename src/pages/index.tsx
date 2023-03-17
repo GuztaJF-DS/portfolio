604e7fbd64f3ab8a7ea79e7b78fde4643c1de691
GuztaJF-DS <noobshippuden13@gmail.com>
2023-03-17 14:56:43 -0300

    feat: organized the main menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Link from 'next/link';
+import Styled from 'styled-components';
+
+const MainContainer = Styled.main`
+  .Link {
+    margin-left:12px;
+  }
+  i {
+      margin-top:10px;
+  }
+`;
 
 export default function Home() {
   return (
     <>
-      <main>
+      <MainContainer>
         <p>
           hi, i&apos;m still building most of this things, so yeah expect some bugs
         </p>
@@ -12,22 +22,31 @@ export default function Home() {
           also, welcome, and feel free to look what i&apos;ve done yet :)
         </p>
         <br/>
-        <Link href={'/dataStructure'}>
+        <i>
+          Studies:
+        </i>
+        <Link href={'/dataStructure'} className={"Link"}>
           Data Structure
         </Link>
-        <Link href={'https://anotherflix.vercel.app/'}>
+        <i>
+          Projects:
+        </i>
+        <Link href={'https://anotherflix.vercel.app/'} className={"Link"}>
           AnotherFlix
         </Link>
-        <Link href={'https://spotifycloneui.netlify.app/'}>
+        <Link href={'https://spotifycloneui.netlify.app/'} className={"Link"}>
           Spotify Ui Clone - (Old project, not really planning on finishing this one)
         </Link>
-        <Link href={'https://github.com/GuztaJF-DS'}>
+        <i>
+          Links:
+        </i>
+        <Link href={'https://github.com/GuztaJF-DS'} className={"Link"}>
           My Github
         </Link>
-        <Link href={'https://leetcode.com/noobshippuden13/'}>
+        <Link href={'https://leetcode.com/noobshippuden13/'} className={"Link"}>
           My LeetCode Profile
         </Link>
-      </main>
+      </MainContainer>
     </>
   );
 }
